6242c9ba2d5c3e1e9ca9baf4ee78449200d617f5
err <foamy125@gmail.com>
2026-04-18 15:59:43 -0500

    stuff

--- public/js/cljs-runtime/shadow.module.index.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('index');
-
-try { control_plane.ui.core.mount_BANG_(); } catch (e) { console.error("An error occurred when calling (control-plane.ui.core/mount!)"); console.error(e); }
